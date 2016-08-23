13bbd83bf2fe66f5ea98f1eb46ede03db9f35d3b
wander2k <lilingkun@gmail.com>
2016-08-24 00:55:20 +0900

    Bug fix - 1.Enforce article slug 2.Link to sections in header 3.404 page not working
    Some other improvement - 
    1.Error/404 page enhancement
    2.schema.sql critical error :section_id missied in table articles

--- src/FizzBuzz/Controller/Article.php
+++ src/FizzBuzz/Controller/Article.php
@@ -7,12 +7,16 @@ class Article extends AbstractController
     public function view()
     {
         $articleID = (int) $this->getRoutedParam('id');
+        $slug = $this->getRoutedParam('slug');
         if (!$articleID) {
-            throw new \Exception("Article ID is invalid", 404);
+           throw new \Exception("Article ID is invalid", 404);
+        }
+        if (!$slug) {
+        	throw new \Exception("Slug is invalid", 404);
         }
 
         $articlesRepository = $this->app->container->get('ArticlesRepository');
-        $article = $articlesRepository->findById($articleID);
+        $article = $articlesRepository->findByIdAndSlug($articleID, $slug);
 
         if (!is_array($article) or !count($article)) {
             throw new \Exception("Article not found", 404);
--- src/FizzBuzz/Controller/Homepage.php
+++ src/FizzBuzz/Controller/Homepage.php
@@ -27,7 +27,7 @@ class Homepage extends AbstractController
     public function handle404()
     {
         $this->app->response->setStatusCode(404);
-        
+
         $this->tpl->uri = $this->getRoutedParam('uri');
 
         echo $this->tpl->render('404.phtml');
